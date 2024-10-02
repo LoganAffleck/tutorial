0a3631cf3a73203bdabcab7255a7ef2aa3755bf1
J. Logan Affleck <affleck.logan@gmail.com>
2024-10-02 07:51:53 -0400

    Update symbol-preview.js

--- src/app/symbol-preview.js
+++ src/app/symbol-preview.js
@@ -1,23 +1,29 @@
 // pages/edit-symbol.jsx
 // import { builder } from "@builder.io/sdk";
-import { RenderBuilderContent } from "../components/builder";
-import { BuilderComponent, builder } from '@builder.io/react';
+import { builder } from "@builder.io/sdk";
+import { RenderBuilderContent } from "../../components/builder";
 
-// Builder Public API Key set in .env file
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY);
 
-export default async function Page(props) {
-    // const content = await builder
-    //   // Get the page content from Builder with the specified options
-    //   .get("symbol")
-    //   // Convert the result to a promise
-    //   .toPromise();
-  
-    return (
-      <>
-        {/* Render the Builder page */}
-        <RenderBuilderContent model={symbol} />
-        <BuilderComponent model={symbol} />
-      </>
-    );
-  }
+
+export default async function Page() {
+  const builderModelName = "symbol";
+
+  const content = await builder
+    // Get the page content from Builder with the specified options
+    .get(builderModelName, {
+      userAttributes: {
+        // Use the page path specified in the URL to fetch the content
+        urlPath: "/" + (props?.params?.page?.join("/") || ""),
+      },
+    })
+    // Convert the result to a promise
+    .toPromise();
+
+  return (
+    <>
+      {/* Render the Builder page */}
+      <RenderBuilderContent content={content} model={builderModelName} options={{enrich: true}}/>
+    </>
+  );
+}
